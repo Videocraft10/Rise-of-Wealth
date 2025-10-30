7a46786770484da8894122f71781372cca224ed1
Videcraft <90282888+Videocraft10@users.noreply.github.com>
2025-10-30 11:43:35 -0400

    Revert "Prebuilds"
    This reverts commit 6a91a22cddf14436fb57926cc1df884b0887ce94.

--- rise-of-wealth/title_screen.gd
+++ rise-of-wealth/title_screen.gd
@@ -1,21 +1,18 @@
 extends CanvasLayer
 
-@export var next_scene_path: String = "res://scenes/First.tscn"
+@export var next_scene_path: String = "res://scenes/Game.tscn"  # your main game scene
 
+# Node references
 @onready var start_btn: Button = $StartButton
 @onready var quit_btn: Button = $QuitButton
 
 func _ready() -> void:
-	start_btn.pressed.connect(Callable(self, "_on_start_pressed"))
-	quit_btn.pressed.connect(Callable(self, "_on_quit_pressed"))
+	# Connect button signals
+	start_btn.pressed.connect(_on_start_pressed)
+	quit_btn.pressed.connect(_on_quit_pressed)
 
 func _on_start_pressed() -> void:
-	if FileAccess.file_exists(next_scene_path):
-		var err = get_tree().change_scene(next_scene_path)
-		if err != OK:
-			push_error("Failed to change scene to '%s'" % next_scene_path)
-	else:
-		push_error("Scene file not found: '%s'" % next_scene_path)
+	get_tree().change_scene_to_file(next_scene_path)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
